ab4469e02b85a2a58d9f8d45b05faf93b530a8f9
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-08-04 15:44:51 -0500

    N-D Numpy dataset output instead of 2D

--- ImageTools/dicom_to_numpy.py
+++ ImageTools/dicom_to_numpy.py
@@ -4,83 +4,114 @@ import csv, os
 
 
 def pad_image(image, target_dim):
+
+	size = image.GetSize()
+
+
+	if size[2] > target_dim[0] or size[1] > target_dim[1] or size[0] > target_dim[2]:
+		# If the image size is larger than the agreed upon padded size, discard it
+		return None
+
+	#filter = sitk.ResampleImageFilter()
+	#filter.SetOutputOrigin(image.GetOrigin())
+	#filter.SetOutputDirection(image.GetDirection())
+	#filter.SetOutputSpacing([3.0, 3.0, 2.5])
+	#filter.SetInterpolator(sitk.sitkLinear)
+	#image = filter.Execute(image)
+
 	arr = sitk.GetArrayFromImage(image)
 
 	padding_map = []
 	for i in range(len(target_dim)):
 		padding_map.append( [0, max(0, target_dim[i] - arr.shape[i])] )
 
+	# TODO: Resample images that are larger after padding (images with z-spacing < 1.0)
+
 	x = np.pad(arr, padding_map, mode='constant',constant_values=0)
+
+
+	x = np.float32(x)
+	x -= np.mean(x)
+	x /= np.std(x)
+
 	return np.int16(x)
 
 
+shrink_factor = 7
 root_path     = "/home/mostafa/SummerProject/Data/"
 labels_path   = root_path + "labels.csv"
-series_path   = root_path + "series/"
-np_path       = root_path + "np/"
+image_path    = root_path + "/segmented/" + str(shrink_factor) + "/"
+np_path       = root_path + "/np/" + str(shrink_factor) + "/"
 
-shrink_factors= range(2, 4)
-ref_dim       = (612, 512, 512)
+ref_dim       = (630, 512, 512)
 batch_max_size= 3.5 * 1024 * 1024 * 1024 # 3.5 Gigabytes
-batch_max_size= 200 * 1024 * 1024 		 # 900 Megabytes
+# batch_max_size= 200 * 1024 * 1024 		 # 900 Megabytes
+
+
+scale_factor  = 1/shrink_factor
+out_dim = (int(ref_dim[0] * scale_factor), int(ref_dim[1] * scale_factor), int(ref_dim[2] * scale_factor))
 
 cum_batch_size = 0
-for shrink_factor in shrink_factors:
+batch_number = 0
+X = []
+Y = []
+with open(labels_path, 'r') as csvfile:
+	csv_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+	for row in csv_reader:
+		record_id = row['record_id']
+		if row['emphysema'] != '':
+			emphysema = int(row['emphysema'])
+		else:
+			emphysema = 0
 
-	batch_number = 0
-	entries = []
-	scale_factor  = 1/shrink_factor
-	out_dim = (int(ref_dim[0] * scale_factor), int(ref_dim[1] * scale_factor), int(ref_dim[2] * scale_factor))
+		if row['bronchiectasis1'] != '':
+			bronchiectasis = int(row['bronchiectasis1'])
+		else:
+			bronchiectasis = 0
 
-	with open(labels_path, 'r') as csvfile:
-		csv_reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
-		for row in csv_reader:
-			record_id = row['record_id']
-			if row['emphysema'] != '':
-				emphysema = int(row['emphysema'])
-			else:
-				emphysema = 0
+		subject_path = image_path + record_id + ".nrrd"
+		if not os.path.isfile(subject_path):
+			# print("Missing subject {0}".format(subject_path))
+			continue
 
-			if row['bronchiectasis1'] != '':
-				bronchiectasis = int(row['bronchiectasis1'])
-			else:
-				bronchiectasis = 0
+		# print("Processing subject {0})".format(record_id))
 
-			subject_path = series_path + record_id
-			if not os.path.isdir(subject_path):
-				print("Missing subject {0}".format(subject_path))
-				continue
+		image = sitk.ReadImage(subject_path)
+		arr = pad_image(image, out_dim)
+		if arr is None:
+			print("Discarding subject {0}".format(subject_path))
+			continue
 
-			print("Processing subject {0} at shrink factor {1} ({2}x{3}x{4})".format(record_id, shrink_factor, out_dim[0], out_dim[1], out_dim[2]))
-			reader = sitk.ImageSeriesReader()
-			reader.SetFileNames(reader.GetGDCMSeriesFileNames(subject_path))
-			image = reader.Execute()
+		# print("Array size = {0} bytes".format(arr.nbytes))
 
-			image = sitk.Shrink(image, [shrink_factor, shrink_factor, shrink_factor])
+		if cum_batch_size + arr.nbytes > batch_max_size:
 
-			arr = pad_image(image, out_dim)
-			print("Array size = {0} bytes".format(arr.nbytes))
+			print("Batch size reached, starting a new batch")
+			array_to_store = np.array(entries)
+			filename = "data_{0}_{1}".format(shrink_factor, batch_number)
+			print("Writing file " + filename)
+			np.savez(np_path + filename, array_to_store)
+			del array_to_store
+			del entries
+			entries = []
+			cum_batch_size = 0
+			batch_number += 1
 
-			if cum_batch_size + arr.nbytes > batch_max_size:
+		cum_batch_size += arr.nbytes
 
-				print("Batch size reached, starting a new batch")
-				array_to_store = np.array(entries)
-				filename = "data_{0}_{1}".format(shrink_factor, batch_number)
-				print("Writing file " + filename)
-				np.savez(np_path + filename, array_to_store)
-				del array_to_store
-				del entries
-				entries = []
-				cum_batch_size = 0
-				batch_number += 1
+		# Flatten array
+		# arr.shape = (-1)
 
-			cum_batch_size += arr.nbytes
-			print(cum_batch_size)
-			# For simplicity just use emphysema status as the class variable
-			entries.append([arr, emphysema])
+		cls = [0, 0]
+		cls[emphysema] = 1
+		X.append(arr)
+		Y.append(cls)
 
-		arr = np.array(entries)
-		filename = "data_{0}_{1}".format(shrink_factor, batch_number)
-		print("Writing file " + filename)
-		np.savez(np_path + filename, arr)
+	filename = "images_{0}".format(batch_number)
+	print("Writing file " + filename)
+	np.save(np_path + filename, np.array(X))
+
+	filename = "labels_{0}".format(batch_number)
+	print("Writing file " + filename)
+	np.save(np_path + filename, np.array(Y))
 
