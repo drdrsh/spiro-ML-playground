06aa816249f3ee7e7a6699990c35085d056266c0
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-08-04 12:06:57 -0500

    Added multiprocess queuing to the python segmentation runner

--- ImageTools/trimmer_runner.py
+++ ImageTools/trimmer_runner.py
@@ -1,8 +1,32 @@
-import os, sys, subprocess
+import os, sys, subprocess, time
+
+max_process = 8
 
 if len(sys.argv) != 3:
 	print("Usage: timmer_runner.py discrete_directory_name output_directory_name")
 	sys.exit(1)
+
+processes = []
+
+def waitForEmptySpot(processes):
+	while processes:
+		for proc in processes:
+			retcode = proc.poll()
+			if retcode is not None: # Process finished.
+				processes.remove(proc)
+				break
+			else: # No process is done, wait a bit and check again.
+				time.sleep(.1)
+				continue
+
+def waitForEmptyQueue(processes):
+	while processes:
+		for proc in processes:
+			retcode = proc.poll()
+			if retcode is not None: # Process finished.
+				processes.remove(proc)
+			else: # No process is done, wait a bit and check again.
+				time.sleep(.1)
 
 for x in os.walk(sys.argv[1]):
 
@@ -15,4 +39,10 @@ for x in os.walk(sys.argv[1]):
 		full_input_path = os.path.abspath(sys.argv[1] + '/' + file)
 		full_output_path = os.path.abspath(sys.argv[2] + '/' + file)
 		print(full_input_path + " => " + full_output_path)
-		subprocess.call(['./LungSegment', full_input_path, full_output_path])
+		if len(processes) < max_process:
+			p = subprocess.Popen(['./LungSegment', full_input_path, full_output_path])
+			processes.append(p)
+		else:
+			waitForEmptySpot(processes)
+
+	waitForEmptyQueue(processes)
