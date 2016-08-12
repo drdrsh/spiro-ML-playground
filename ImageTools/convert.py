3a3c529af6fd6984bf3a6ed54fbed2336d31ae5c
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-08-12 12:30:46 -0500

    partial cleanup for convert.py

--- ImageTools/convert.py
+++ ImageTools/convert.py
@@ -12,46 +12,24 @@ from APPIL_DNN.process_runner import ProcessRunner
 if len(sys.argv) > 1:
     Config.load(sys.argv[1])
 
-bin_root = Config.get('bin_root')
 shrink_sizes = [5, 7]
 
 
-input_subtype  = 'segmented' if segment_enabled  else 'discrete'
-input_path  = CLI.get_path('train', input_subtype,  active_shrink_factor)
-
-output_subtype = 'segmented_augmented' if segment_enabled  else 'augmented'
-output_path = CLI.get_path('train', output_subtype, active_shrink_factor)
-
-try:
-    os.mkdir(output_path)
-except:
-    pass
-
-num_examples, num_classes, labels_table = APPIL_DNN.data.get_labels()
-
-input_files = glob.glob(input_path + '/' + "*.nrrd")
-
-# import os, sys, subprocess
-
-if len(sys.argv) != 3:
-	print("Usage: runner.py series_directory_name output_directory_name")
-	sys.exit(1)
-
-for x in os.walk(sys.argv[1]):
+for x in os.walk(sys.argv[2]):
     basename = os.path.basename(x[0])
     if basename == '.' or basename == '':
         continue
 
-    full_input_path = os.path.abspath(sys.argv[1] + '/' + basename)
-    full_output_path = os.path.abspath(sys.argv[2] + '/')
+    full_input_path = os.path.abspath(sys.argv[2] + '/' + basename)
+    full_output_path = os.path.abspath(sys.argv[3] + '/')
 
     try:
         os.mkdir(os.path.abspath(sys.argv[2]))
     except:
         pass
 
-    params =
-    params.extend
-    subprocess.call(
-        ['./Debug/ImageMultiRes', '--dicom', full_input_path, '--output', full_output_path]
-                    )
+    exe_path = Config.get('bin_root') + '/ImageMultiRes'
+    params = [exe_path, '--dicom', full_input_path, '--output', full_output_path]
+    params.extend(shrink_sizes)
+    print(params)
+    subprocess.call(params)
