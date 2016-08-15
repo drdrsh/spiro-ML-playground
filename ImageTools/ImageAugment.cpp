d6ddd656f4d777b7020d4de42cff277765dae481
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-08-15 16:56:46 -0500

    Added Shear , Fixed TPS and introduced probability based randomization

--- ImageTools/ImageAugment.cpp
+++ ImageTools/ImageAugment.cpp
@@ -16,21 +16,16 @@
 #include <itkThinPlateSplineKernelTransform.h>
 #include <itkImageRandomConstIteratorWithIndex.h>
 #include <itkAdditiveGaussianNoiseImageFilter.h>
-
-
+#include <itkImageDuplicator.h>
 
 #include <boost/filesystem.hpp>
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
+#include <boost/random/discrete_distribution.hpp>
 
 
-#ifdef _WIN32
-#include <direct.h>
-#elif defined __linux__
-#include <sys/stat.h>
-#endif
 
 typedef signed short    PixelType;
 typedef   double        CoordinateRepType;
@@ -62,6 +57,14 @@ bool file_exists(std::string filename) {
     return boost::filesystem::exists(filename) && boost::filesystem::file_size(filename) != 0;
 }
 
+ImageType::Pointer duplicate_image(ImageType::Pointer inputImage) {
+    typedef itk::ImageDuplicator< ImageType > DuplicatorType;
+    DuplicatorType::Pointer duplicator = DuplicatorType::New();
+    duplicator->SetInputImage(inputImage);
+    duplicator->Update();
+    return duplicator->GetOutput();
+}
+
 void save_image(ImageType::Pointer image, std::string outputFilename) {
 	WriterType::Pointer writer = WriterType::New();
 	writer->SetFileName(outputFilename);
@@ -70,6 +73,40 @@ void save_image(ImageType::Pointer image, std::string outputFilename) {
 	std::cout << std::endl << "Writing: " << outputFilename << std::endl;
 	writer->Update();
 }
+
+ImageType::RegionType get_central_region(ImageType::Pointer inputImage) {
+
+    ImageType::SizeType imageSize = inputImage->GetLargestPossibleRegion().GetSize();
+
+    ImageType::IndexType regionStart;
+    ImageType::SizeType regionSize;
+
+    regionStart[0] = (imageSize[0] / 10);
+    regionStart[1] = (imageSize[0] / 10);
+    regionStart[2] = (imageSize[0] / 15);
+
+    regionSize[0] = (imageSize[0] - (regionStart[0] * 2));
+    regionSize[1] = (imageSize[1] - (regionStart[1] * 2));
+    regionSize[2] = (imageSize[2] - (regionStart[2] * 2));
+
+    ImageType::RegionType region;
+    region.SetIndex(regionStart);
+    region.SetSize(regionSize);
+
+    return region;
+
+}
+
+ImageType::Pointer get_central_image(ImageType::Pointer image) {
+
+    ImageType::Pointer inputImage = duplicate_image(image);
+    ImageType::SizeType imageSize = inputImage->GetLargestPossibleRegion().GetSize();
+    inputImage->SetRequestedRegion(get_central_region(inputImage));
+    inputImage->Update();
+    return inputImage;
+
+}
+
 
 ResampleImageFilterType::Pointer get_default_resampler(ImageType::Pointer inputImage) {
 
@@ -84,15 +121,74 @@ ResampleImageFilterType::Pointer get_default_resampler(ImageType::Pointer inputI
 
 }
 
-void deform_rotate(ImageType::Pointer inputImage, std::string outputFilename) {
+void deform_shear(ImageType::Pointer inputImage, std::string outputFilename) {
 
-    if(file_exists(outputFilename)) {
-        std::cout << std::endl <<  outputFilename << " already exists, exiting " <<  std::endl;
-        return;
+    float low_end  = .1f;
+    float high_end = .3f;
+
+    boost::random::mt19937 rng(time(0));
+
+    boost::random::uniform_int_distribution<> idd(-50, 50);
+
+    float coeff = (high_end - low_end) * ((float)idd(rng) / 50.0f);
+    coeff += low_end;
+
+    boost::random::uniform_int_distribution<> axis_idd(0, 2);
+    int axis1 = axis_idd(rng);
+    int axis2 = -1;
+
+    switch(axis1) {
+        case 0:
+            axis2 = 1;
+            break;
+        case 1:
+            axis2 = 2;
+            break;
+        case 2:
+            axis2 = 0;
+            break;
     }
 
-	float degRange = 10.0f;
-	boost::random::mt19937 rng;
+    AffineTransformType::Pointer transform = AffineTransformType::New();
+    /*
+
+    const ImageType::SpacingType & spacing = inputImage->GetSpacing();
+    const ImageType::PointType & origin = inputImage->GetOrigin();
+    ImageType::SizeType size = inputImage->GetLargestPossibleRegion().GetSize();
+
+    double center[3];
+    for (int i = 0; i < Dimension; i++) {
+        center[i] = origin[i] + spacing[i] * size[i] / 2.0;
+    }
+
+    AffineTransformType::OutputVectorType translation1;
+    for (int i = 0; i < Dimension; i++) {
+        translation1[i] = -center[i];
+    }
+    transform->Translate(translation1);
+   */
+    //const double degreesToRadians = std::atan(1.0) / 45.0;
+    //double angle = coeff * degreesToRadians;
+    transform->Shear(axis1, axis2,  coeff, false);
+    /*
+    AffineTransformType::OutputVectorType translation2;
+    for (int i = 0; i < Dimension; i++) {
+        translation2[i] = center[i];
+    }
+    transform->Translate(translation2);
+    */
+
+    ResampleImageFilterType::Pointer resampler = get_default_resampler(inputImage);
+    resampler->SetTransform(transform);
+    save_image(resampler->GetOutput(), outputFilename);
+
+}
+
+void deform_rotate(ImageType::Pointer inputImage, std::string outputFilename) {
+
+
+	float degRange = 5.0f;
+	boost::random::mt19937 rng(time(0));
 	boost::random::uniform_int_distribution<> idd(-50, 50);
 	float angles[3] = {
 		degRange * ((float)idd(rng) / 50.0f),
@@ -140,17 +236,13 @@ void deform_rotate(ImageType::Pointer inputImage, std::string outputFilename) {
 
 void deform_noise(ImageType::Pointer inputImage, std::string outputFilename) {
 
-    if(file_exists(outputFilename)) {
-        std::cout << std::endl <<  outputFilename << " already exists, exiting " <<  std::endl;
-        return;
-    }
-
-
     float std = 100.0f;
 	NoiseFilterType::Pointer filter = NoiseFilterType::New();
 
 	filter->SetStandardDeviation(std);
 	filter->SetInput(inputImage);
+    filter->GetOutput()->SetRequestedRegion(get_central_region(inputImage));
+
 	save_image(filter->GetOutput(), outputFilename);
 }
 
@@ -158,47 +250,57 @@ void deform_histogram(ImageType::Pointer inputImage, std::string outputFilename)
 
 }
 
+
 void deform_tps(ImageType::Pointer inputImage, std::string outputFilename) {
 
-    if(file_exists(outputFilename)) {
-        std::cout << std::endl <<  outputFilename << " already exists, exiting " <<  std::endl;
-        return;
-    }
 
-	boost::random::mt19937 rng;
+
+	boost::random::mt19937 rng(time(0));
 	boost::random::uniform_int_distribution<> iid(-100, 100);
-	
-	float deform_factor = 0.01f * (iid(rng) / 100)
+
+    float rnd = (float)iid(rng);
+	float deform_factor = 0.3f * (float)( (rnd + 1) / 100.0f);
 	unsigned int deform_landmarks = 500;
 
-	
+    ImageType::SizeType imageSize = inputImage->GetLargestPossibleRegion().GetSize();
 
-	PointSetType::Pointer sourceLandMarks = PointSetType::New();
+
+    PointSetType::Pointer sourceLandMarks = PointSetType::New();
 	PointSetType::Pointer targetLandMarks = PointSetType::New();
-	PointType p1;     PointType p2;
-	PointSetType::PointsContainer::Pointer sourceLandMarkContainer =
-		sourceLandMarks->GetPoints();
-	PointSetType::PointsContainer::Pointer targetLandMarkContainer =
-		targetLandMarks->GetPoints();
+	PointSetType::PointsContainer::Pointer sourceLandMarkContainer = sourceLandMarks->GetPoints();
+	PointSetType::PointsContainer::Pointer targetLandMarkContainer = targetLandMarks->GetPoints();
 
 	PointIdType id = itk::NumericTraits< PointIdType >::ZeroValue();
 
-	itk::ImageRandomConstIteratorWithIndex<ImageType> it(inputImage, inputImage->GetLargestPossibleRegion());
-	it.SetNumberOfSamples(deform_landmarks);
-	it.GoToBegin();
+    ImageType::SpacingType sp = inputImage->GetSpacing();
+	itk::ImageRandomConstIteratorWithIndex<ImageType> it(inputImage, get_central_region(inputImage) );
+
+    it.SetNumberOfSamples(deform_landmarks);
+    it.GoToBegin();
 	while (!it.IsAtEnd()) {
-		PixelType t = it.Get();
+
+        PointType p1;
+        PointType p2;
+
 		ImageType::IndexType idx = it.GetIndex();
-		ImageType::PointType pnt;
+		PointType pnt;
 		inputImage->TransformIndexToPhysicalPoint(idx, pnt);
-		p1 = pnt;
-		p2 = pnt;
+        p1 = pnt;
+        p2 = pnt;
+        double randF = deform_factor * iid(rng);
+        for(int i=0;i <Dimension; i++) {
+            p2[i] += randF;
+            ImageType::IndexType midx;
+            inputImage->TransformPhysicalPointToIndex(p2, midx);
+            if(midx[i] > imageSize[i]) {
+                p2[i] -= (randF * 2);
+            }
+        }
 
-		p2[0] += deform_factor * iid(rng);
-		p2[1] += deform_factor * iid(rng);
-		p2[2] += deform_factor * iid(rng);
+        //std::cout << p1[0] << " " << p1[1] << " " << p1[2] << std::endl;
+        //std::cout << p2[0] << " " << p2[1] << " " << p2[2] << std::endl;
 
-		sourceLandMarkContainer->InsertElement(id, p1);
+        sourceLandMarkContainer->InsertElement(id,   p1);
 		targetLandMarkContainer->InsertElement(id++, p2);
 
 		++it;
@@ -211,6 +313,7 @@ void deform_tps(ImageType::Pointer inputImage, std::string outputFilename) {
 
 	ResampleImageFilterType::Pointer resampler = get_default_resampler(inputImage);
 	resampler->SetTransform(tps);
+
 	save_image(resampler->GetOutput(), outputFilename);
 }
 
@@ -257,19 +360,25 @@ int main(int argc, const char* argv[]) {
 		
 		ImageType::Pointer inputImage = reader->GetOutput();
 
-		boost::filesystem::path filenameParts(inputFilename);
+        boost::filesystem::path filenameParts(inputFilename);
 		std::string basename = filenameParts.stem().generic_string();
 
-		boost::random::mt19937 rng;
-		boost::random::uniform_int_distribution<> operationDice(0, 2);
+		boost::random::mt19937 rng(time(0));
 
-		enum operation {NOISE, TPS, ROTATE};
+        double op_prob[] = {0.1, 0.4, 0.4, 0.1};
+        boost::random::discrete_distribution<unsigned int> operationDice(op_prob);
+		enum operation {NOISE, TPS, ROTATE, SHEAR};
 
 		ThreadVector threads;
 		for (unsigned int i = 0; i < augment_count; i++) {
 
 			int op = operationDice(rng);
 			std::string outputFilename = boost::filesystem::path(outputDirName + "/" + basename + "_" + std::to_string(i) + ".nrrd").generic_string();
+            std::cout << "Performing opetration " << op << " on image " << i << std::endl;
+            if(file_exists(outputFilename)) {
+                std::cout << std::endl <<  outputFilename << " already exists, exiting " <<  std::endl;
+                continue;
+            }
 			switch (op) {
 				case NOISE:
 					threads.push_back(std::thread(deform_noise, inputImage, outputFilename));
@@ -277,9 +386,12 @@ int main(int argc, const char* argv[]) {
 				case TPS:
 					threads.push_back(std::thread(deform_tps, inputImage, outputFilename));
 					break;
-				case ROTATE:
-					threads.push_back(std::thread(deform_rotate, inputImage, outputFilename));
-					break;
+                case ROTATE:
+                    threads.push_back(std::thread(deform_rotate, inputImage, outputFilename));
+                    break;
+                case SHEAR:
+                    threads.push_back(std::thread(deform_shear, inputImage, outputFilename));
+                    break;
 			}
 		}
 
