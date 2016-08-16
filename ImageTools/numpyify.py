ead6ac15191bc9cb7c99f3f96f448605af0c556e
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-08-16 12:26:54 -0500

    Added test file handling

--- ImageTools/numpyify.py
+++ ImageTools/numpyify.py
@@ -37,18 +37,27 @@ def write_batch(X, Y, batch_number, np_path):
 def pad_image(image, target_dim):
 
 	size = image.GetSize()
-	if size[2] > target_dim[0]:
 
-		to_trim = size[2] - target_dim[0]
-		if  int( (to_trim * 100) / target_dim[0]) >= 25:
+	trim_start_list = [0, 0, 0]
+	trim_end_list = [0, 0, 0]
+
+
+	for i in range(len(size)):
+
+		to_trim = 0
+		if size[i] > target_dim[i]:
+			to_trim = size[i] - target_dim[i]
+		if  int( (to_trim * 100) / target_dim[i]) >= 25:
 			# Difference between image size and target size is different by more than 25%
 			# Trimming will likely destroy image data, discard the image altogether
 			return None
 
 		trim_start = int(to_trim / 2)
 		trim_end   = trim_start + (to_trim % 2)
+		trim_start_list[i] = trim_start
+		trim_end_list[i] = trim_end
 
-		image = sitk.Crop(image, [0, 0, trim_start], [0, 0, trim_end] )
+	image = sitk.Crop(image, trim_start_list, trim_end_list)
 
 	arr = sitk.GetArrayFromImage(image)
 
@@ -68,6 +77,71 @@ def pad_image(image, target_dim):
 	return np.int16(x)
 
 
+def process_path(input_path, output_path, labels_table, num_classes, out_dim) :
+
+	images = glob.glob(input_path + '/' + "*.nrrd")
+	random.shuffle(images)
+
+	files_done = 0
+	batches_written = 0
+	total_files = len(images)
+
+	cum_batch_size = 0
+	batch_number = 0
+	cls = list(0 for i in range(num_classes))
+	X = []
+	Y = []
+
+	assert len(images) != 0
+
+	for image_path in images:
+
+		update_cli(total_files, files_done, batches_written)
+
+		# print("Processing subject {0})".format(record_id))
+		record_id = ((os.path.splitext(os.path.basename(image_path))[0]).split('_'))[0]
+		label = labels_table[record_id]
+		cls[label] = 1
+
+		try:
+			image = sitk.ReadImage(image_path)
+		except(RuntimeError):
+			#print("Invalid image {0}".format(image_path))
+			files_done += 1
+			continue
+
+		arr = pad_image(image, out_dim)
+		if arr is None:
+			#print("Discarding subject {0}\n".format(record_id))
+			files_done += 1
+			continue
+
+		# print("Array size = {0} bytes".format(arr.nbytes))
+
+		if cum_batch_size + arr.nbytes > batch_max_size:
+
+			# print("Batch size reached, starting a new batch")
+			write_batch(X, Y, batch_number, output_path)
+			X = []
+			Y = []
+			cum_batch_size = 0
+			batch_number += 1
+			batches_written += 1
+
+		cum_batch_size += arr.nbytes
+
+		X.append(arr)
+		Y.append(cls)
+
+		files_done += 1
+
+	# print("Writing last batch")
+	write_batch(X, Y, batch_number, output_path)
+	batches_written += 1
+	update_cli(total_files, files_done, batches_written)
+	print("\nDone!\n")
+
+
 if len(sys.argv) > 1:
 	Config.load(sys.argv[1])
 
@@ -81,83 +155,39 @@ segment_enabled = Config.get('segment_enabled')
 active_shrink_factor = Config.get('active_shrink_factor')
 batch_max_size = Config.get('batch_max_size')
 
-
-input_subtype  = 'segmented_augmented' if segment_enabled  else 'raw_augmented'
-input_path  = CLI.get_path(current_mode , input_subtype,  active_shrink_factor)
-
-output_path = CLI.get_path(current_mode , input_subtype + '_np', active_shrink_factor)
-
+output_dimensions = None
 try:
-	os.makedirs(output_path)
-except FileExistsError:
+	# This will override the calculate dimensions, note that dimensions will be reduced by trimming from both sides
+	output_dimensions = tuple(Config.get('output_dimensions'))
+except KeyError:
 	pass
-
 
 num_examples, num_classes, labels_table = APPIL_DNN.data.get_labels()
 
 scale_factor  = 1/active_shrink_factor
-out_dim = (int(ref_dim[0] * scale_factor), int(ref_dim[1] * scale_factor), int(ref_dim[2] * scale_factor))
 
+out_dim = (int(ref_dim[0] * scale_factor), int(ref_dim[1] * scale_factor), int(ref_dim[2] * scale_factor))
+if output_dimensions is not None:
+	out_dim = output_dimensions
 
 print('\nOutput image size is ' + str(out_dim) + '\n')
-images = glob.glob(input_path + '/' + "*.nrrd")
-random.shuffle(images)
 
-files_done = 0
-batches_written = 0
-total_files = len(images)
+runs = {
+	'train': ['raw_augmented', 'segmented_augmented'],
+	'test' : ['raw', 'segmented']
+}
 
-cum_batch_size = 0
-batch_number = 0
-cls = list(0 for i in range(num_classes))
-X = []
-Y = []
+for i in runs:
 
-assert len(images) != 0
+	print('\nProcessing {0} data\n'.format(i))
 
-for image_path in images:
-
-	update_cli(total_files, files_done, batches_written)
-
-	# print("Processing subject {0})".format(record_id))
-	record_id = ((os.path.splitext(os.path.basename(image_path))[0]).split('_'))[0]
-	label = labels_table[record_id]
-	cls[label] = 1
-
+	input_type = i
+	mode = 1 if segment_enabled else 0
+	input_subtype = runs[i][mode]
+	input_path  = CLI.get_path(i , input_subtype,  active_shrink_factor)
+	output_path = CLI.get_path(i, input_subtype + '_np', active_shrink_factor)
 	try:
-		image = sitk.ReadImage(image_path)
-	except(RuntimeError):
-		#print("Invalid image {0}".format(image_path))
-		files_done += 1
-		continue
-
-	arr = pad_image(image, out_dim)
-	if arr is None:
-		#print("Discarding subject {0}\n".format(record_id))
-		files_done += 1
-		continue
-
-	# print("Array size = {0} bytes".format(arr.nbytes))
-
-	if cum_batch_size + arr.nbytes > batch_max_size:
-
-		# print("Batch size reached, starting a new batch")
-		write_batch(X, Y, batch_number, output_path)
-		X = []
-		Y = []
-		cum_batch_size = 0
-		batch_number += 1
-		batches_written += 1
-
-	cum_batch_size += arr.nbytes
-
-	X.append(arr)
-	Y.append(cls)
-
-	files_done += 1
-
-# print("Writing last batch")
-write_batch(X, Y, batch_number, output_path)
-batches_written += 1
-update_cli(total_files, files_done, batches_written)
-print("\nDone!\n")
+		os.makedirs(output_path)
+	except FileExistsError:
+		pass
+	process_path(input_path, output_path, labels_table, num_classes, out_dim)
