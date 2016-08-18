942777ce75f9caf0a1c7e76579fb54dc66fa4630
Mostafa Abdelraouf <mostafa.mohmmed@gmail.com>
2016-08-17 19:24:21 -0500

    Added Input data visualization

---
+++ myshow.py
@@ -0,0 +1,132 @@
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+from ipywidgets import interact, interactive
+from ipywidgets import widgets
+
+def random_tile(ds, grid_size=(10, 10)):
+
+    arr = np.copy(ds.X)
+    arr.shape = ds.original_X_shape
+    entries = list(range(arr.shape[0]))
+    random_indices = random.sample(entries, grid_size[0] * grid_size[1])
+
+    selected_slice = int(arr.shape[3] / 2)
+    images = []
+    for i in range(grid_size[0]):
+        for j in range(grid_size[1]):
+            counter = i * grid_size[0] + j
+            idx = random_indices[counter]
+            slc = arr[idx, selected_slice, :,:]
+            img = sitk.GetImageFromArray(slc)
+            images.append(img)
+
+    image = sitk.GetArrayFromImage(sitk.Tile(images, grid_size))
+    fig_size = (20, 20)
+    fig = plt.figure(figsize=fig_size, dpi=40)
+    plt.set_cmap("gray")
+    plt.imshow(image)
+
+
+
+
+    
+
+def myshow(img, title=None, margin=0.05, dpi=20 ):
+    nda = sitk.GetArrayFromImage(img)
+
+    spacing = img.GetSpacing()
+    slicer = False
+
+    if nda.ndim == 3:
+        # fastest dim, either component or x
+        c = nda.shape[-1]
+
+        # the the number of components is 3 or 4 consider it an RGB image
+        if not c in (3,4):
+            slicer = True
+
+    elif nda.ndim == 4:
+        c = nda.shape[-1]
+
+        if not c in (3,4):
+            raise Runtime("Unable to show 3D-vector Image")
+
+        # take a z-slice
+        slicer = True
+
+    if (slicer):
+        ysize = nda.shape[1]
+        xsize = nda.shape[2]
+    else:
+        ysize = nda.shape[0]
+        xsize = nda.shape[1]
+
+
+    # Make a figure big enough to accomodate an axis of xpixels by ypixels
+    # as well as the ticklabels, etc...
+    figsize = (1 + margin) * ysize / dpi, (1 + margin) * xsize / dpi
+    def callback(z=None):
+
+        extent = (0, xsize*spacing[1], ysize*spacing[0], 0)
+
+        fig = plt.figure(figsize=figsize, dpi=dpi)
+
+        # Make the axis the right size...
+        ax = fig.add_axes([margin, margin, 1 - 2*margin, 1 - 2*margin])
+
+        plt.set_cmap("gray")
+
+        if z is None:
+            ax.imshow(nda,extent=extent,interpolation=None)
+        else:
+            ax.imshow(nda[z,...],extent=extent,interpolation=None)
+
+        if title:
+            plt.title(title)
+
+        plt.show()
+
+    if slicer:
+        interact(callback, z=(0,nda.shape[0]-1))
+    else:
+        callback()
+
+def myshow3d(img, xslices=[], yslices=[], zslices=[], title=None, margin=0.05, dpi=80):
+    size = img.GetSize()
+    img_xslices = [img[s,:,:] for s in xslices]
+    img_yslices = [img[:,s,:] for s in yslices]
+    img_zslices = [img[:,:,s] for s in zslices]
+
+    maxlen = max(len(img_xslices), len(img_yslices), len(img_zslices))
+
+
+    img_null = sitk.Image([0,0], img.GetPixelIDValue(), img.GetNumberOfComponentsPerPixel())
+
+    img_slices = []
+    d = 0
+
+    if len(img_xslices):
+        img_slices += img_xslices + [img_null]*(maxlen-len(img_xslices))
+        d += 1
+
+    if len(img_yslices):
+        img_slices += img_yslices + [img_null]*(maxlen-len(img_yslices))
+        d += 1
+
+    if len(img_zslices):
+        img_slices += img_zslices + [img_null]*(maxlen-len(img_zslices))
+        d +=1
+
+    if maxlen != 0:
+        if img.GetNumberOfComponentsPerPixel() == 1:
+            img = sitk.Tile(img_slices, [maxlen,d])
+        #TODO check in code to get Tile Filter working with VectorImages
+        else:
+            img_comps = []
+            for i in range(0,img.GetNumberOfComponentsPerPixel()):
+                img_slices_c = [sitk.VectorIndexSelectionCast(s, i) for s in img_slices]
+                img_comps.append(sitk.Tile(img_slices_c, [maxlen,d]))
+            img = sitk.Compose(img_comps)
